c5659f3b534b80b530b2611fe3c5943ec5f80b94
NoeDemange <demange.noe@outlook.fr>
2023-02-15 17:06:03 +0100

    Essaie de aitken

--- code-Noé/Aitken/main.c
+++ code-Noé/Aitken/main.c
@@ -46,16 +46,27 @@ int main(int argc, char *argv[]){
     
     //méthode puissance
 
-    //double x[] = {0.1,0.1,0.2,0,0.1,0.15,0.15,0.2};
-    //double newX[] = {0.1,0.1,0.2,0,0.1,0.15,0.15,0.2};
+    //variable
     double x[nb_sommets];
+    double x1[nb_sommets];
+    double x2[nb_sommets];
     double newX[nb_sommets];
+    double g[nb_sommets];
+    double h[nb_sommets]; 
+    double inter = 0;
+    ARC *A;
+    double s=0;
+    double s1=0;
 
+    //Initialisation de x
     for(int j=0;j<nb_sommets;j++){
         x[j] = 1.0/nb_sommets;
-        newX[j] = x[j];
+        x1[j] = 0.0;
+        x2[j] = 0.0;
+        newX[j] = 0.0;
+        g[j] = 0.0;
+        h[j] = 0.0;
     }
-
 
     double norme;
     double c = 0.0;
@@ -63,10 +74,8 @@ int main(int argc, char *argv[]){
     do{
         norme = 0.0;
         //p = multi_VecMat(Tsommets,nb_sommets,f);
-        double inter;
-        ARC *A;
-        double s=0;
-        double s1=0;
+        s=0;
+        s1=0;
         for(int i=0;i<nb_sommets;i++){
             if(f[i]==1){
                 s+=x[i]*f[i];
@@ -82,7 +91,61 @@ int main(int argc, char *argv[]){
             }
             newX[i] = alpha*inter+s1;
         }
-        //aff_vec(nb_sommets);
+
+        //Aitken
+        if((cpt+1)%2==0){
+
+            //calcul de x1
+            s=0;
+            s1=0;
+            for(int i=0;i<nb_sommets;i++){
+                if(f[i]==1){
+                    s+=x[i]*f[i];
+                }
+            }
+            s1=(s*alpha+(1-alpha))/nb_sommets;
+            for(int i=0;i<nb_sommets;i++){
+                A = &Tsommets[i];
+                inter = 0;
+                while(A!=NULL){
+                    inter += A->weight*x[A->pred-1];
+                    A = A->suiv;
+                }
+                x1[i] = alpha*inter+s1;
+            }
+
+            //calcul de x2
+            s=0;
+            s1=0;
+            for(int i=0;i<nb_sommets;i++){
+                if(f[i]==1){
+                    s+=x1[i]*f[i];
+                }
+            }
+            s1=(s*alpha+(1-alpha))/nb_sommets;
+            for(int i=0;i<nb_sommets;i++){
+                A = &Tsommets[i];
+                inter = 0;
+                while(A!=NULL){
+                    inter += A->weight*x1[A->pred-1];
+                    A = A->suiv;
+                }
+                x2[i] = alpha*inter+s1;
+            }
+//printf("x1\n"); aff_vec(nb_sommets, x1);
+//printf("x\n"); aff_vec(nb_sommets, x);
+            for(int i=0;i<nb_sommets;i++){
+                g[i]= (x1[i]-x[i])*(x1[i]-x[i]);
+                h[i]= x2[i]-(2*x1[i])+x[i];
+                newX[i]= x[i]-(g[i]/h[i]);
+                //printf("rapport %f et i %d et newx %f\n",g[i]/h[i],i, newX[i]);
+            }
+           //printf("g\n"); aff_vec(nb_sommets, g);    
+           //printf("h\n"); aff_vec(nb_sommets, h);
+           //printf("newx\n"); aff_vec(nb_sommets, newX);
+        }
+
+        //printf("newx\n");aff_vec(nb_sommets, newX);
         for(int i=0;i<nb_sommets;i++){
             if((newX[i]-x[i])>0){
                 c = newX[i]-x[i];}
@@ -90,10 +153,10 @@ int main(int argc, char *argv[]){
         norme+=c;
         x[i] = newX[i];
         }
-        //printf("%lf\n",norme);
+        printf("norme : %lf\n",norme);
         cpt++;
     }while(norme>EPSILON);
     printf("Nb itération : %d\n",cpt);
-    //aff_vec(nb_sommets, newX);
+    //aff_vec(nb_sommets, x);
     exit(0);
 }
